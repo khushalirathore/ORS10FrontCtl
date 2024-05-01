2bf075936c8ab71ef43ddedb86f7e1a1bd4d1352
khushali rathore <khushalirathore813@mail.com>
2024-05-01 12:29:00 +0530

    save

---
+++ projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ * Khushali Rathore 
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
